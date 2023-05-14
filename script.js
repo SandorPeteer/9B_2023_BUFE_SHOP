733320b95309fd84925c2d51f71aa006c21784d6
zacharabel15 <113335722+zacharabel15@users.noreply.github.com>
2023-05-14 20:56:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-var Rántotthús = 220;
-var DuplaHamburger = 650;
+var Sajtburger = 120;
+var Cheddar = 650;
+var Bbqhamburger = 300;
+var Olaszoshamburger = 400;
+var Lyonihotdog = 200;
+var Newyorkhotdog = 460;
+var Amerikaihotdog = 600;
+var Simahotdog = 200;
+var Csirkesszendvics = 750;
+var Vegaszendvics = 400;
+var Gyrosbagett = 800;
+var Royalbagett = 650;
 
 function aktualis_ar() {
-   document.getElementById('rantotthusi_').innerHTML = Rántotthús + " Ft";
-   document.getElementById('duplahambi_').innerHTML = DuplaHamburger + " Ft";
+   document.getElementById('sajtburger_').innerHTML = Sajtburger + " Ft";
+   document.getElementById('cheddar_').innerHTML = Cheddar + " Ft";
+   document.getElementById('bbqhamburger_').innerHTML = Bbqhamburger + " Ft";
+   document.getElementById('olaszoshamburger_').innerHTML = Olaszoshamburger + " Ft";
+   document.getElementById('lyonihotdog_').innerHTML = Lyonihotdog + " Ft";
+   document.getElementById('newyorkhotdog_').innerHTML = Newyorkhotdog + " Ft";
+   document.getElementById('amerikaihotdog_').innerHTML = Amerikaihotdog + " Ft";
+   document.getElementById('simahotdog_').innerHTML = Simahotdog + " Ft";
+   document.getElementById('csirkesszendvics').innerHTML = Csirkesszendvics + " Ft";
+   document.getElementById('vegaszendvics_').innerHTML = Vegaszendvics + " Ft";
+   document.getElementById('gyrosbagett_').innerHTML = Gyrosbagett + " Ft";
+   document.getElementById('royalbagett_').innerHTML = Royalbagett + " Ft";
+
+
+
 }
 
 function kosarba(termek, darab) {
