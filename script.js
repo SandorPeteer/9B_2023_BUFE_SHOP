ee4fbdf5b38d62bcee80188fd5ce966986485b1a
ZavarkoZeno <113335827+ZavarkoZeno@users.noreply.github.com>
2023-05-16 12:23:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var Sajtburger = 120;
 var Cheddar = 650;
 var Bbqhamburger = 300;
 var Olaszoshamburger = 400;
+var Minipizza = 250;
+var Sultkrumpli = 100;
+var Franciapiritos = 150;
 var Lyonihotdog = 200;
 var Newyorkhotdog = 460;
 var Amerikaihotdog = 600;
@@ -11,16 +14,15 @@ var Vegaszendvics = 400;
 var Gyrosbagett = 800;
 var Royalbagett = 650;
 var MarsCsoki = 150;
-var Xixoszilva = 400;
-var Xixomango = 400;
-
-var SajtosChips = 300;
 
 function aktualis_ar() {
    document.getElementById('sajtburger_').innerHTML = Sajtburger + " Ft";
    document.getElementById('cheddar_').innerHTML = Cheddar + " Ft";
    document.getElementById('bbqhamburger_').innerHTML = Bbqhamburger + " Ft";
    document.getElementById('olaszoshamburger_').innerHTML = Olaszoshamburger + " Ft";
+   document.getElementById('minipizza_').innerHTML = Minipizza + " Ft";
+   document.getElementById('sultkrumpli_').innerHTML = Sultkrumpli + " Ft";
+   document.getElementById('franciapiritos_').innerHTML = Franciapiritos + " Ft";
    document.getElementById('lyonihotdog_').innerHTML = Lyonihotdog + " Ft";
    document.getElementById('newyorkhotdog_').innerHTML = Newyorkhotdog + " Ft";
    document.getElementById('amerikaihotdog_').innerHTML = Amerikaihotdog + " Ft";
@@ -30,10 +32,6 @@ function aktualis_ar() {
    document.getElementById('gyrosbagett_').innerHTML = Gyrosbagett + " Ft";
    document.getElementById('royalbagett_').innerHTML = Royalbagett + " Ft";
    document.getElementById('mars_csokika_').innerHTML = MarsCsoki + " Ft";
-   document.getElementById('xixoszilva_').innerHTML = Xixoszilva + " Ft";
-   document.getElementById('xixomango_').innerHTML = Xixomango + " Ft";
-
-      document.getElementById('sajtoschips_').innerHTML = SajtosChips + " Ft";
 
 
 
